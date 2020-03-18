fa96b7914c136ece046191113370a74dff16fa9c
jdrd <davidricodias.dev@gmail.com>
2020-03-18 14:57:27 +0100

    Config

--- MPalabrados1/nbproject/Makefile-variables.mk
+++ MPalabrados1/nbproject/Makefile-variables.mk
@@ -7,21 +7,21 @@ CND_BASEDIR=`pwd`
 CND_BUILDDIR=build
 CND_DISTDIR=dist
 # Debug configuration
-CND_PLATFORM_Debug=None-Linux
-CND_ARTIFACT_DIR_Debug=dist/Debug/None-Linux
+CND_PLATFORM_Debug=GNU-Linux
+CND_ARTIFACT_DIR_Debug=dist/Debug/GNU-Linux
 CND_ARTIFACT_NAME_Debug=mpalabrados1
-CND_ARTIFACT_PATH_Debug=dist/Debug/None-Linux/mpalabrados1
-CND_PACKAGE_DIR_Debug=dist/Debug/None-Linux/package
+CND_ARTIFACT_PATH_Debug=dist/Debug/GNU-Linux/mpalabrados1
+CND_PACKAGE_DIR_Debug=dist/Debug/GNU-Linux/package
 CND_PACKAGE_NAME_Debug=mpalabrados1.tar
-CND_PACKAGE_PATH_Debug=dist/Debug/None-Linux/package/mpalabrados1.tar
+CND_PACKAGE_PATH_Debug=dist/Debug/GNU-Linux/package/mpalabrados1.tar
 # Release configuration
-CND_PLATFORM_Release=None-Linux
-CND_ARTIFACT_DIR_Release=dist/Release/None-Linux
+CND_PLATFORM_Release=GNU-Linux
+CND_ARTIFACT_DIR_Release=dist/Release/GNU-Linux
 CND_ARTIFACT_NAME_Release=mpalabrados1
-CND_ARTIFACT_PATH_Release=dist/Release/None-Linux/mpalabrados1
-CND_PACKAGE_DIR_Release=dist/Release/None-Linux/package
+CND_ARTIFACT_PATH_Release=dist/Release/GNU-Linux/mpalabrados1
+CND_PACKAGE_DIR_Release=dist/Release/GNU-Linux/package
 CND_PACKAGE_NAME_Release=mpalabrados1.tar
-CND_PACKAGE_PATH_Release=dist/Release/None-Linux/package/mpalabrados1.tar
+CND_PACKAGE_PATH_Release=dist/Release/GNU-Linux/package/mpalabrados1.tar
 #
 # include compiler specific variables
 #
