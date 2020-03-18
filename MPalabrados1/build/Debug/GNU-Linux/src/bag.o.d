93a0f4418a5eb3478e46b464a5816595d8fc584b
jdrd <davidricodias.dev@gmail.com>
2020-03-18 19:27:47 +0100

    Borrado objetos

--- MPalabrados1/build/Debug/GNU-Linux/src/bag.o.d
+++
@@ -1,8 +0,0 @@
-build/Debug/GNU-Linux/src/bag.o: src/bag.cpp include/bag.h \
- ../local/include/language.h ../local/include/wordlist.h
-
-include/bag.h:
-
-../local/include/language.h:
-
-../local/include/wordlist.h:
--- MPalabrados1/build/Debug/GNU-Linux/src/main.o.d
+++
@@ -1,8 +0,0 @@
-build/Debug/GNU-Linux/src/main.o: src/main.cpp include/bag.h \
- ../local/include/language.h ../local/include/wordlist.h
-
-include/bag.h:
-
-../local/include/language.h:
-
-../local/include/wordlist.h:
