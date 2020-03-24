515f2e2c04e8cccf1ab163f920122d7216ed6413
jdrd <davidricodias.dev@gmail.com>
2020-03-24 18:20:57 +0100

    Update

---
+++ MPalabrados1/src/player.cpp
@@ -0,0 +1,29 @@
+/**
+ * @file player.cpp
+ * @author DECSAI
+ * @note To be implemented by students
+ */
+
+#include <cstring>
+#include "player.h"
+///@warning complete missing #includes
+/// See http://www.cplusplus.com/reference/cstring/ for details about cstrings
+using namespace std;
+
+/**
+ * @brief Removes a position from a cstring
+ * @param cstr The cstring
+ * @param pos The position
+ * @return The cstring is modified
+ * @warning To be fully implemented
+ */
+void removeCString(char *cstr, int pos);
+
+/**
+ * @brief Sort a cstring from A to Z
+ * @param cstr The cstring
+ * @return The cstring is modified
+ * @warning To be fully implemented
+ */
+void sortCString(char *cstr);
+
